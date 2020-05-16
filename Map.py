0d34ac987f4df7c717e58c6ef7f7c9dce2e52831
Giselle Martel <gmartel90@gmail.com>
2020-05-16 17:03:38 -0400

    fix bug in threshold calc

--- Map.py
+++ Map.py
@@ -5,7 +5,6 @@
 # --------------------------------------------------------
 
 import geopandas as gpd
-from shapely.geometry import Polygon
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,7 +17,7 @@ def init():
     plt.rcParams.update({'figure.dpi': 200})
     matplotlib.use("TkAgg")
 
-    crime_map = createMapFromShpFile("./Shape/crime_dt.shp", 0.002, 50)
+    crime_map = createMapFromShpFile("./Shape/crime_dt.shp", 0.002, 1)
 
     generateCrimeMapDataForEachCell(crime_map)
     generateStandardDeviation(crime_map)
@@ -49,25 +48,37 @@ def createMapFromShpFile(file, step_size, threshold):
     total_bounds = data.total_bounds
 
     # get the horizontal-vertical size of grid (X*Y)
-    grid_size = calcNumGridCells(total_bounds, step_size)
+    grid_dimensions = calcGridDimensions(total_bounds, step_size)
+
+    # calculate the threshold based on given percentage and 2D area of grid
+    threshold_index = int(grid_dimensions[0] * grid_dimensions[1] * (1 - threshold)) - 1
+
+    # use matplotlib hist2d function to plot grid with given step size
+    crime_map = plt.hist2d(x_values, y_values, bins=grid_dimensions, cmap=cmap)
+
+    num_crimes_per_cell_lst = np.array(crime_map[0]).flatten()
+    num_crimes_per_cell_lst.sort()
+
+    # threshold_val
+    threshold_val = num_crimes_per_cell_lst[threshold_index]
 
     # determine the norm of the grid based on the given threshold
-    grid_norm = BoundaryNorm([threshold], ncolors=cmap.N)
+    grid_norm = BoundaryNorm([threshold_val], ncolors=cmap.N)
 
-    # use matplotlib hist2d function to plot grid with given threshold and step size
-    crime_map = plt.hist2d(x_values, y_values, bins=grid_size, cmap=cmap, norm=grid_norm)
+    # use matplotlib hist2d function to plot grid with given step size and threshold value
+    crime_map = plt.hist2d(x_values, y_values, bins=grid_dimensions, cmap=cmap, norm=grid_norm)
 
     return crime_map
 
 
-def calcNumGridCells(total_bounds, step_size):
+def calcGridDimensions(total_bounds, step_size):
     # get the bounds of the whole crime area
     min_x = total_bounds[0]
     min_y = total_bounds[1]
     max_x = total_bounds[2]
     max_y = total_bounds[3]
 
-    # get num cells in grid based on bounds of crime area and step size
+    # get num cells on x and y axis of grid based on bounds of crime area and step size
     x_grid_steps = np.ceil((max_x - min_x) / step_size)
     y_grid_steps = np.ceil((max_y - min_y) / step_size)
 
@@ -80,12 +91,14 @@ def generateCrimeMapDataForEachCell(crime_map):
     xedges = crime_map[1]
     yedges = crime_map[2]
     xy_vertices = []
+    sum = 0
     for (x, xi) in zip(xedges, range(0, len(xedges) - 1)):
         for (y, yi) in zip(yedges, range(0, len(yedges) - 1)):
             xy_vertices.append([x, y])
             plt.text(x + 0.002 / 2, y + 0.002 / 2, str(vals[xi][yi]),
                      fontdict=dict(fontsize=3, ha='center', va='center'))
-    print(vals)
+            sum += vals[xi][yi]
+    print(sum)
 
 
 def generateStandardDeviation(crime_map):
